2d16a10b7c14cf332af8262f6382edf3bc6e6667
poketorena <poketorena@users.noreply.github.com>
2018-10-18 15:43:12 +0900

    BMIを求めるプログラム

--- IntroductoryProgrammingWithCSharp/Program.cs
+++ IntroductoryProgrammingWithCSharp/Program.cs
@@ -10,12 +10,13 @@ namespace IntroductoryProgrammingWithCSharp
     {
         static void Main(string[] args)
         {
-            Console.Write("半径を入力してください: ");
-            double x = double.Parse(Console.ReadLine());
+            Console.Write("身長[cm] = ");
+            double height = double.Parse(Console.ReadLine());
+            Console.Write("体重[kg] = ");
+            double weight = double.Parse(Console.ReadLine());
 
-            double area = x * x * Math.PI;
-
-            Console.Write("半径{0}の円の面積は{1}\n", x, area);
+            double bmi = weight / (height * height / 10000);
+            Console.WriteLine("BMI = {0}", bmi);
         }
     }
 }
