4aa542451d5370b43c281d0f1c6122d1c02e3cbb
poketorena <poketorena@users.noreply.github.com>
2018-10-18 15:47:57 +0900

    奇数か偶数かを判定するプログラム

--- IntroductoryProgrammingWithCSharp/Program.cs
+++ IntroductoryProgrammingWithCSharp/Program.cs
@@ -10,36 +10,15 @@ namespace IntroductoryProgrammingWithCSharp
     {
         static void Main(string[] args)
         {
-            // 整数を2つ入力してもらう
-            int x, y;
-            Console.Write("1つ目の整数を入力してください : ");
-            x = int.Parse(Console.ReadLine());
-            Console.Write("2つ目の整数を入力してください : ");
-            y = int.Parse(Console.ReadLine());
-
-            // + - / * のいずれかを入力してもらう
-            char op;
-            Console.Write("行いたい操作を入力してください(+ - / *) : ");
-            op = Console.ReadLine()[0];
-
-            switch (op)
+            Console.WriteLine("整数を入力してください");
+            int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
             {
-                case '+':
-                    Console.Write("{0} + {1} = {2}", x, y, x + y);
-                    break;
-                case '-':
-                    Console.Write("{0} - {1} = {2}", x, y, x - y);
-                    break;
-                case '*':
-                    Console.Write("{0} × {1} = {2}", x, y, x * y);
-                    break;
-                case '/':
-                    if (y != 0)
-                        Console.Write("{0} ÷ {1} = {2} … {3}", x, y, x / y, x % y);
-                    break;
-                default:
-                    Console.Write("対応していない操作です");
-                    break;
+                Console.WriteLine("偶数だよ");
+            }
+            else
+            {
+                Console.WriteLine("奇数だよ");
             }
         }
     }
