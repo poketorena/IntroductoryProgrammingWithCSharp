18bdf8a94ec553821459420b92a2a237a87d6c0c
poketorena <poketorena@users.noreply.github.com>
2018-10-18 15:48:46 +0900

    BMIを求めるプログラム（改良版）

--- IntroductoryProgrammingWithCSharp/Program.cs
+++ IntroductoryProgrammingWithCSharp/Program.cs
@@ -10,15 +10,32 @@ namespace IntroductoryProgrammingWithCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("整数を入力してください");
-            int x = int.Parse(Console.ReadLine());
-            if (x % 2 == 0)
+            Console.Write("身長[cm] = ");
+            double height = double.Parse(Console.ReadLine());
+            Console.Write("体重[kg] = ");
+            double weight = double.Parse(Console.ReadLine());
+
+            double bmi = weight / (height * height / 10000);
+
+            if (bmi < 19.8)
             {
-                Console.WriteLine("偶数だよ");
+                Console.WriteLine("痩せ型です");
+            }
+            else if (bmi < 24.2)
+            {
+                Console.WriteLine("普通です");
+            }
+            else if (bmi < 26.4)
+            {
+                Console.WriteLine("やや肥満（過体重）です");
+            }
+            else if (bmi < 35.0)
+            {
+                Console.WriteLine("肥満です");
             }
             else
             {
-                Console.WriteLine("奇数だよ");
+                Console.WriteLine("高度肥満（要治療）です");
             }
         }
     }
