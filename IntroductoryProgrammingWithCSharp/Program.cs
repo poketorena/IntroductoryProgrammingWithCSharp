4b9810ddde073497173d0d4de3f7aa54f9f09bd5
poketorena <poketorena@users.noreply.github.com>
2018-10-18 15:41:10 +0900

    標準入出力と四則演算

--- IntroductoryProgrammingWithCSharp/Program.cs
+++ IntroductoryProgrammingWithCSharp/Program.cs
@@ -10,9 +10,19 @@ namespace IntroductoryProgrammingWithCSharp
     {
         static void Main(string[] args)
         {
-            var x = Console.ReadLine();
-            Console.WriteLine(x);
-            Console.WriteLine(x);
+            double x, y, z;
+
+            Console.WriteLine("input x :");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("input y :");
+            y = double.Parse(Console.ReadLine());
+
+            z = x * x + y * y;
+            x /= z;
+            y /= -z;
+
+            Console.WriteLine("({0},{1})", x, y);
         }
     }
 }
